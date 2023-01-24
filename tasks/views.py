ac4452dc64cf47a1a850f0d68875a7a59c28569f
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-01-24 16:13:29 +0100

    adding layout templates to task app

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,9 @@ tasks = ["foo", "bar", "baz"]
 
 # Create your views here.
 def index(request):
-    return render(reuest, "tasks/index.html", {
+    return render(request, "tasks/index.html", {
         "tasks": tasks
     })
+
+def add(request):
+    return render(request, "tasks/add.html")
